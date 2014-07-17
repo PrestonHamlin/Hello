1965524b7eedf5e487176e81e0a0938dbb0ec86d
Preston <pwh11@my.fsu.edu>
2014-07-16 20:18:34 -0400

    started CUDA hello

---
+++ languages/CUDA/hello.cu
@@ -0,0 +1,110 @@
+// Code by Preston Hamlin
+
+// See header file for detailed comments.
+
+#include "hello.h"
+
+
+
+
+int main() {
+  int cuda_status;
+  int num_devs;
+  //const int ARRAY_SIZE = 1<<20;   // one million elements
+  
+  // error code is cudaErrorNoDevice if no suitable devices exist
+  if ((cuda_status = cudaGetDeviceCount(&num_devs)) != cudaSuccess) {
+    FatalError("cudaGetDeviceCount", cuda_status);
+  }
+  else printf("\nDevices: %d\n", num_devs);
+
+  int a[]   = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
+  int* d_a  = 0;
+
+
+  // attempt to allocate memory on device
+  if ((cuda_status = cudaMalloc(&d_a, sizeof(int) * 16)) != cudaSuccess) {
+    FatalError("cudaDeviceSynchronize", cuda_status);
+  }
+  cudaMemcpy(d_a, a, sizeof(int) * 16, cudaMemcpyHostToDevice);
+
+  printf("\n=== Before ===\n");
+  for (int i=0; i<16; ++i) printf("%d\n", a[i]);
+
+  printf("\n=== After Step 1 ===\n");
+  d_ScanOpReduce<<<1, dim3(16, 16)>>>(d_a, 4, 0);
+  cudaMemcpy(a, d_a, sizeof(int) * 16, cudaMemcpyDeviceToHost);
+  for (int i=0; i<16; ++i) printf("%d\n", a[i]);
+
+
+  printf("\n=== After Step 2 ===\n");
+
+
+
+  // sync any remaining buffers (stdout for example)
+  if ((cuda_status = cudaDeviceSynchronize()) != cudaSuccess) {
+    FatalError("cudaDeviceSynchronize", cuda_status);
+  }
+
+
+  cudaFree(d_a);
+  printf("\nHave a nice day.\n");
+  return 0;
+}
+
+
+
+
+
+
+//============================================================================
+//                      UserSpace Functions & Wrappers
+//============================================================================
+
+void FatalError(const char* msg, int e) {
+  printf("\nERROR: %s\n", msg);
+  if (e) printf("\tcode: %d\n", e);
+  exit(1);
+}
+__host__ __device__ void NormalError(const char* msg, int e) {
+  printf("\nERROR: %s\n", msg);
+  if (e) printf("\tcode: %d\n", e);
+}
+
+
+
+
+
+//============================================================================
+//                              CUDA Kernels
+//============================================================================
+
+// Simply prints the unique ID of each thread.
+//  Used for checking the ordering of thread launch ranges
+__global__ void CPrintID() {
+  int blocknum, threadnum;
+ 
+  blocknum =  blockIdx.x + 
+              blockIdx.y*gridDim.x + 
+              blockIdx.z*gridDim.x*gridDim.y;
+
+  threadnum = threadIdx.x +
+              threadIdx.y*blockDim.x + 
+              threadIdx.z*blockDim.x*blockDim.y;
+
+  printf("B%d/%d [%d/%d/%d] \t- T%d/%d [%d/%d/%d] \t- %d\n",
+          blocknum,
+          gridDim.x * gridDim.y * gridDim.z,
+          blockIdx.x, blockIdx.y, blockIdx.z,
+
+          threadnum,
+          blockDim.x * blockDim.y * blockDim.z,
+          threadIdx.x, threadIdx.y, threadIdx.z,
+
+          blocknum*(blockDim.x * blockDim.y * blockDim.z) + threadnum
+          );
+}
+
+
+
+
---
+++ languages/CUDA/hello.h
@@ -0,0 +1,271 @@
+/*  Code by Preston Hamlin
+This file provides some sample CUDA code. CUDA is used for GPGPU programming.
+  The code is meant to be a "hello world" of sorts for CUDA, since the scan
+  algorithm is one of the first one learns when exploring GPGPU.
+
+The program contained herein provides for and demonstrates a scan algorithm.
+  The scan (or prefix-sum) algorithm implemened here is the Blelloch variant.
+  A scan algorithm takes an array of values (typically integers) and performs
+  sucessive operations such that for each element a[i] in the array, a[i] is set
+  to a[i-1] OP a[i]. This has the effect of a "running total."
+
+  An example scan addition:
+  before: [0, 1, 2, 3, 4,  5,  6,  7,  8,  9]
+  after:  [0, 1, 3, 6, 10, 15, 21, 28, 36, 45]
+
+Here, a[0] is the start term, so it is OPed with the identity value, which is 0
+  for addition. Meaning, a[0] = a[0] + 0, or a[0] = 0 + 0 = 0.
+  The following are the remaining steps:
+  a[1] = a[0] + a[1]    = 0 + 1   = 1
+  a[2] = a[1] + a[2]    = 1 + 2   = 3
+  a[3] = a[2] + a[3]    = 3 + 3   = 6
+  a[4] = a[3] + a[4]    = 6 + 4   = 10
+  a[5] = 10 + 5  = 15
+  a[6] = 15 + 6  = 21
+  a[7] = 21 + 7  = 28
+  a[8] = 28 + 8  = 36
+  a[9] = 36 + 45 = 45
+
+  A max scan would maintain the maximum value encountered:
+  before: [5, 12, 47, 16, 3,  48, 12, 55, 67, 3]
+  after:  [5, 12, 47, 47, 47, 48, 48, 55, 67, 67]
+
+One benefit of the concept is that algorithms exist for parallel computation.
+  For any binary associative operator (addition, multiplication, union, etc...)
+  the computations can be reordered and still yield the same result.
+  1 + 2 + 3 + 4 would often be calculated by a human as
+  ((1 + 2) + 3) + 4
+
+  If the addition were to be right-associaive, however, it would look like
+  1 + (2 + (3 + 4))
+
+  Being of interest for parallel computation is
+  (1 + 2) + (3 + 4)
+
+  As is plainly visible, there are two components which can be computed 
+  independently of one another. This independence resulting from the grouping
+  of terms around an associative operator is what is utilized in the first half
+  of the Blelloch algorithm. If only the end result is desired, then the first
+  half will suffice, as the last element of the array will contain the result.
+  For the cases where the intermediate results (the "running total") is also
+  desired, the second step of the algorithm should also be utilized.
+
+  Blelloch's algorithm used a divide-and-conquer approach to divide the array
+  into a tree and then collect the value of lesser brances at intervals.
+
+  Example run of Blelloch's scan algorithm with addition operator:
+  0 1   1 0   1 1   1 0   0 1   0 1   1 1   0 1 
+   \|    \|    \|    \|    \|    \|    \|    \|  
+    1     1     2     1     1     1     2     1
+      \   |       \   |       \   |       \   |
+        \ |         \ |         \ |         \ |
+          2           3           2           3
+              \       |               \       |
+                  \   |                   \   |
+                      5                       5
+                            \                 |
+                                  \           |
+                                        \     |
+                                              10
+
+  The elements in the array do indeed add up to 10. The elements in this array
+  also happen to be the beginning of the Champernowne constant in binary, with
+  a few bits cut off to have the array size be a power of 2.
+
+  After the first step, the array looks like
+  0 1   1 2   1 2   1 5   0 1   0 2   1 2   0 10
+
+  This would be problematic for when the running total is needed. The second
+  step of the algorithm takes the existing intermediate results and populates
+  the remainder of the array. To facilitate the algorithm, the last term is set
+  to 0 which is the identity value for addition.
+
+  For each iteration, the right entry is moved to the left, and the left entry
+  is OPed with the right entry and moved to the right.
+  a[i] = right            a[7]  = 0
+  a[j] = left + right     a[15] = 5 + 0
+  
+  0 1   1 2   1 2   1 5   0 1   0 2   1 2   0 0
+                       \                     /
+                            \           /
+                                  X
+                            /           \
+                       /                     \
+  0 1   1 2   1 2   1 0   0 1   0 2   1 2   0 5
+           \         /             \         /
+                X                       X
+           /         \             /         \
+  0 1   1 0   1 2   1 2   0 1   0 5   1 2   0 7
+     \   /       \   /       \   /       \   /
+       X           X           X           X
+     /   \       /   \       /   \       /   \
+  0 0   1 1   1 2   1 4   0 5   0 6   1 7   0 9
+   X     X     X     X     X     X     X     X
+  0 0   1 2   2 3   4 5   5 5   6 6   7 8   9 9
+
+  Note that the resulting array seems a bit off. This is because Blelloch's
+  algotithm is an exclusive scan, meaning that the value at each position in
+  the result array reflects the running total up to that point not including
+  that spot in the original data array. At index 2 there has been only one bit
+  (remember that this is Champernowne's constant in binary) seen at index 1 so
+  the exclusive scan has a value of 1 at that index. Similiarly at index 11 the
+  sum of all the prior values is 6.
+
+  To convert an exclusive scan to an inclusive one (that is, one representing
+  the running total up to and including the current index) one can simply shift
+  all values in the array to the left by one space and replace the last element
+  in the array with the final total. One can either parse out the total before
+  starting the second half of the algorithm, or hold on to the last element in
+  the original data array and sum it with the second-to-last element of the
+  result array.
+
+  TODO: second half of Blelloch algorithm
+
+  TODO: wrapper functions
+    TODO: optimize thread usage
+    TODO: split oddly sized arrays into power-of-two chunks
+
+  TODO: version of scan optimized for latency/occupancy/perormance
+
+  TODO: exceptions
+
+*/
+
+
+#include <cuda_runtime.h>
+#include <device_launch_parameters.h>
+#include <curand.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+#define SCAN_OP_ADD 0
+#define SCAN_OP_SUB 1
+#define SCAN_OP_MUL 2
+#define SCAN_OP_MIN 3
+#define SCAN_OP_MAX 4
+#define SCAN_OP_XOR 5
+
+
+// declarations
+void FatalError(const char* msg, int e=0);
+__host__ __device__ void NormalError(const char* msg, int e=0);
+__global__ void CPrintID();
+
+
+// templated declarations
+template <class T>
+__device__ void d_ScanApplyOp(T* x, T* y, int mode);
+template <class T>
+__global__ void d_ScanOpReduce(T* d_a, int iters, int mode);
+
+
+
+
+
+
+//============================================================================
+//                          Templated Functions
+//============================================================================
+
+/* d_ScanOpReduce - Reduce half of a scan operation
+This is the first half of a scan operation. If only the resulting total or
+  final value is desired, then one can use this without the second half. Else
+  the other half should be used to populate the remainder of the array with
+  the intermediate values.
+
+  T* d_a    - some numeric data type
+  int iters - number of iterations to perform op, equal to lg of the array size
+  int mode  - integer value describing operation (add, max, XOR, etc...) to use
+
+  TODO: multi-block synchonization?
+*/
+template <class T>
+__global__ void d_ScanOpReduce(T* d_a, int iters, int mode) {
+  const int size = (1 << iters);
+  int loc = 0, loc1 = 0, loc2 = 0;
+
+  if (!d_a) {
+    NormalError("NULL array passed to d_ScanOpReduce");
+    return;
+  }
+
+
+  loc = threadIdx.x +
+        threadIdx.y*blockDim.x + 
+        threadIdx.z*blockDim.x*blockDim.y;
+
+  for (int i=0; i<iters; ++i) {
+    loc1 = (((2*loc)+1) * (1<<i)) - 1;
+    loc2 = ((2*(loc+1)) * (1<<i)) - 1;
+
+    if (loc2 < size) {  // if valid desination, perform op
+      printf("%d - %d -> %d\n", i, loc1, loc2);
+      
+      //d_ScanApplyOp(d_a[loc1], d_a[loc2], mode);
+      switch (mode) {
+        case SCAN_OP_ADD:
+          d_a[loc2] = (d_a[loc1] + d_a[loc2]);
+          break;
+        // subtraction is simply the first term minus all others 
+        //  negate d_a[0], scan add, negate all elements in array
+        case SCAN_OP_SUB: 
+          d_a[loc2] = (d_a[loc1] + d_a[loc2]);
+          break;
+        case SCAN_OP_MUL:
+          d_a[loc2] = (d_a[loc1] * d_a[loc2]);
+          break;
+        case SCAN_OP_MIN:
+          d_a[loc2] = (d_a[loc1] > d_a[loc2]) ? d_a[loc2] : d_a[loc1];
+          break;
+        case SCAN_OP_MAX:
+          d_a[loc2] = (d_a[loc1] < d_a[loc2]) ? d_a[loc2] : d_a[loc1];
+          break;
+        case SCAN_OP_XOR:
+          d_a[loc2] = (d_a[loc1] ^ d_a[loc2]);
+          break;
+        default:
+          NormalError("unknown scan op");
+          break;
+      }
+    }
+    __syncthreads();  // synchronizes threads within block
+  }
+
+}
+
+// This function performs various operaions on two values in a device array. It
+//  could be left inside d_ScanOp, but is extracted for readability. There are
+//  too few levels of indirection to make function pointers worthwhile.
+template <class T>
+__device__ void d_ScanApplyOp(T* x, T* y, int mode) {
+  switch (mode) {
+    case SCAN_OP_ADD:
+      *y = (*x + *y);
+      break;
+    // subtraction is simply the first term minus all others 
+    //  negate d_a[0], scan add, negate all elements in array
+    case SCAN_OP_SUB: 
+      *y = (*x + *y);
+      break;
+    case SCAN_OP_MUL:
+      *y = (*x * *y);
+      break;
+    case SCAN_OP_MIN:
+      *y = (*x > *y) ? *y : *x;
+      break;
+    case SCAN_OP_MAX:
+      *y = (*x < *y) ? *y : *x;
+      break;
+    case SCAN_OP_XOR:
+      *y = (*x ^ *y);
+      break;
+    default:
+      NormalError("unknown scan op");
+      break;
+  }
+}
+
+
